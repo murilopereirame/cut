37c915305055b242465949f1a0fbbc55317d08d8
Murilo Pereira <34790144+murilopereirame@users.noreply.github.com>
2024-06-16 03:00:07 +0200

    [FRONT] Changed footer to something less formal

--- svelte/src/components/Footer.svelte
+++ svelte/src/components/Footer.svelte
@@ -7,34 +7,24 @@
 		visible = !visible
 	}
 </script>
-
-<nav class="{$$restProps.class || ''} hidden w-full justify-center gap-5 py-2 bg-pelorous-600 text-white md:flex">
-	<a href="/">Shorten</a>
-	<a href="/">Report URL</a>
-	<p class="font-bold">Copyright © 2024 CUT. All rights reserved.</p>
-	<a href="/">Terms of Use</a>
-	<a href="/">Privacy Policy</a>
-</nav>
-<nav class="{$$restProps.class || ''} w-full flex flex-col py-2 bg-pelorous-600 text-white md:hidden">
-	{#if visible}
-		<div
-			transition:slide
-			class="transition-all delay-150 duration-300 overflow-hidden">
-			<div class="flex flex-col items-center">
-				<a href="/">Shorten</a>
-				<a href="/">Report URL</a>
-				<a href="/">Terms of Use</a>
-				<a href="/">Privacy Policy</a>
-			</div>
-		</div>
-	{/if}
+<nav class="{$$restProps.class || ''} w-full flex flex-col-reverse md:flex-col py-2 bg-pelorous-600 text-white">
+    <div
+        transition:slide
+        class="{visible ? 'max-h-32' : 'max-h-0'} md:flex justify-center transition-all delay-150 duration-300 overflow-hidden md:max-h-fit">
+        <div class="flex flex-col items-center md:flex-row md:gap-5">
+            <a href="/">Shorten</a>
+            <a href="/">Report URL</a>
+            <a href="/">Terms of Use</a>
+            <a href="/">Privacy Policy</a>
+        </div>
+    </div>
 	<h2 class="my-1 w-full flex justify-center">
 		<button
 			class="group relative flex items-center text-left text-base transition "
 			type="button" on:click={toggleVisibility}>
-			<span class="mr-2 font-bold">Copyright © 2024 CUT. All rights reserved.</span>
+			<span class="mr-2 font-bold">Made with <span class="text-sm">❤</span> by 0x6D70</span>
 			<span
-				class="ms-auto h-5 w-5 shrink-0">
+				class="ms-auto h-5 w-5 shrink-0 md:hidden">
           <Hamburger />
         </span>
 		</button>
